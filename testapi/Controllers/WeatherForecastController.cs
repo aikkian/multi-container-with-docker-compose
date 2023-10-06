ac1dbe7be1889ed312bcd9c6f3967b38a2465993
aikkian <aikkian@gmail.com>
2023-10-06 10:47:27 +0800

    first commit

---
+++ testapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace testapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            HttpClient httpClient = new HttpClient();
+            var ab = httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://testapi2/weatherforecast"));
+            var cd = ab.Result;
+            string str = cd.Content.ToString();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
